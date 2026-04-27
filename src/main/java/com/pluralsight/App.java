5a82ca35d4df55e908aa83ad360dd49d2e1fd87f
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-27 14:19:41 -0500

    Util Packages added, transaction examples CSV added, and implemented Home screen with its respective switch case

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,7 +1,66 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+//       Establishing scanner for future use
+        Scanner keyboard = new Scanner(System.in);
+//        Accepts the CSV file and reads it
+        FileReader filereader = new FileReader("TransactionExample.csv");
+        BufferedReader bufferedReader = new BufferedReader(filereader);
+//Starts program
+        boolean programIsRunning = true;
+
+//        Home Screen
+        System.out.println("Booting up...");
+        System.out.println("System checks running...");
+        System.out.println("All tests have been cleared...INITIATING");
+        System.out.println("""
+                D) Add Deposit
+                P) Make Payment (Debit)
+                L) Ledger
+                X) Exit
+                """);
+
+//        Add code to turn char into lowercased
+        char command = keyboard.next().charAt(0);
+        System.out.println(command);
+
+        char d = 0;
+        char p = 1;
+        char l = 2;
+        char x = 3;
+
+        switch (command){
+            case d -> addDeposit();
+            case p -> makePayment();
+            case l -> accessLedger();
+            case x ->  programIsRunning = false;
+        }
 
     }
+//    Ledger Screen (accessLedger Screen)
+    private static void accessLedger() {
+        System.out.println("""
+                    A) All
+                    D) Deposits
+                    P) Payments
+                    R) Reports
+                    H) Home
+                    """);
+    }
+//                BONUS | Custom Reports Screen
+//        System.out.println("""
+//                1) Month To Date
+//                2) Previous Month
+//                3) Year to Date
+//                4) Previous Year
+//                5) Search by Vendor
+//                0) Back
+//                """);
 }
---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Transaction {
+
+        private String date = "";
+        private String time = "";
+        private String description = "";
+        private String location = "";
+        private float amount = 0;
+    }
