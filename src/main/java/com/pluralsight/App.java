0ae1af493cb5b553d505aa3b00b3f8468410e630
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-29 22:22:59 -0500

    Fix mix up of payment and deposit. Prototyping a visual cluterless model in Excess java. Will most likely remove since trivial, too much upkeep, and possibly no use case. Also reinstated ledger switch case along w/methods.

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,9 +1,8 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -15,7 +14,7 @@ public class App {
     public App() throws FileNotFoundException {
     }
 
-
+    static ArrayList<Transaction> transactions = new ArrayList<>();
     //       Establishing scanner for future use----------
     static Scanner keyboard = new Scanner(System.in);
     //        Need to calculate (if there's a) negative amount on the account. If there is offer payment opt else sout N/A
@@ -24,45 +23,42 @@ public class App {
 
 
     public static void main(String[] args) throws InterruptedException, FileNotFoundException {
-//       Establishing clock for future use---------MOVE INTO THE PROPER METHOD WHEN USED
-        LocalDateTime today = LocalDateTime.now();
-        TimeZone timeZone = TimeZone.getTimeZone("US/Central");
-//        System.out.println("(Ex)Today is: " + formattedDate1);
-//
+        LocalDateTime today = LocalDateTime.now();/*Establishing clock for future use---------MOVE INTO THE PROPER METHOD WHEN USED*/
+        TimeZone timeZone = TimeZone.getTimeZone("US/Central");/*System.out.println("(Ex)Today is: " + formattedDate1);/*To test formats*/
 
+        boolean programIsRunning = true;/*Starts program*/
 
-//        Starts program
-        boolean programIsRunning = true;
+        while (programIsRunning) {
 
-//        Home Screen
-        System.out.println("Booting up...");
-        Thread.sleep(2000);
-        System.out.println("System checks running...");
-        Thread.sleep(3000);
-        System.out.println("All tests have been cleared...INITIATING");
-        Thread.sleep(1000);
-        System.out.println("""
-                D) Add Deposit
-                P) Make Payment (Debit)
-                L) Access Ledger
-                X) Exit
-                """);
-        System.out.println("Choose: ");
+            System.out.println("Booting up...");/*Home Screen---------------------------------------------------------*/
+//            Thread.sleep(2000);
+            System.out.println("System checks running...");
+//            Thread.sleep(3000);
+            System.out.println("All tests have been cleared...INITIATING");
+//            Thread.sleep(1000);
+            System.out.println("""
+                    D) Add Deposit
+                    P) Make Payment (Debit)
+                    L) Access Ledger
+                    X) Exit
+                    """);
+            System.out.println("Choose: ");
 
-        char command = keyboard.next().toLowerCase().charAt(0);
+            char command = keyboard.next().toLowerCase().charAt(0);
 
-        switch (command) {
-            case 'd' -> addDeposit();
-            case 'p' -> makePayment();
-            case 'l' -> accessLedger();
-//            case 'h' -> thismainhomescreen();
-            case 'x' -> programIsRunning = false;
-        }
+            switch (command) {
+                case 'd' -> addDeposit();
+                case 'p' -> makePayment();
+                case 'l' -> accessLedger();
+                case 'x' -> programIsRunning = false;
+                default -> System.out.println("Invalid Input, please try again.");
+
+            }/*Closes switch case*/
+        }/*Closes while loop*/
         keyboard.close();
-    }
+    }/*Closes main method*/
 
-    //    Ledger Screen (accessLedger Screen)--------------------------------------------------------------------
-    private static void accessLedger() {
+    private static void accessLedger() {/*Ledger Screen (accessLedger Screen)--------------------------------------------------------------------*/
         boolean inaccessLedger = true;
         while (inaccessLedger) {
             System.out.println("""
@@ -75,27 +71,30 @@ public class App {
             System.out.println("Choose: ");
             char secondCommand = keyboard.next().toLowerCase().charAt(0);
             switch (secondCommand) {
-//                case 'a' -> ;
-//                case 'd' -> ;
-//                case 'p' -> ;
-//                case 'r' -> ;
+                case 'a' -> accessLedgerAll();
+                case 'd' -> accessLedgerDeposits();
+                case 'p' -> accessLedgerPayments();
+                case 'r' -> accessLedgerReports();
                 case 'h' -> inaccessLedger = false;
+                default -> System.out.println("Invalid Input, please try again.");
+
             }
 
         }
 
     }
-    private static void accessLedgerAll(){
-        System.out.println("");
-        String transactionLine = bufferedReader.readLine();
-        int number = 0;
 
-        while (transactionLine != null) {
-            System.out.print(number++ + ". ");
-            System.out.println(transactionLine);
-            transactionLine = bufferedReader.readLine();
+    private static void accessLedgerAll() {
 
-        }
+    }
+    private static void accessLedgerDeposits() {
+
+    }
+    private static void accessLedgerPayments() {
+
+    }
+    private static void accessLedgerReports() {
+
     }
 //                BONUS | Custom Reports Screen
 //        System.out.println("""
@@ -107,51 +106,41 @@ public class App {
 //                0) Back
 //                """();
 
-    //    Payment Screen (makePayment Screen)---------------------------------------------------------------------
-    private static void makePayment() {
+    private static void makePayment() throws InterruptedException {/*Payment Screen (makePayment Screen)---------------------------------------------------------------------*/
         boolean inmakePayment = true;
+        while (inmakePayment) {
+            LocalDateTime today = LocalDateTime.now();/*prompt user for the deposit information and save it to the csv file | For flavor, have them add acct number and signature*/
+            DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy/MM/dd");/*Date format for CSV appendage*/
+            String formatDate1 = today.format(formatter1);
+            DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("hh:mm:ss");/*Time format for CSV appendage*/
+            String formatTime2 = today.format(formatter2);
 
-//         prompt user for the debit information and save it to the csv file
+            String paymentVendor = "Bank";/*Vendor format for CSV appendage - Establishing info/variables (DATE/TIME/DESCRIPTION/VENDOR/AMOUNT) needed for appending later*/
+            String paymentDescription = "Check Deposit";/*Descriptor format for CSV appendage*/
 
+            System.out.println("To make a payment via check, please fill out the fields below.");
+            System.out.println("Enter amount as it appears on your check: $");
+            double paymentCheckAmount = keyboard.nextDouble();
+            keyboard.nextLine(); /*That one error where it jumps over occurs here without this line. Single Carriage Missed Line*/
+            System.out.println("Finally, type your name to digitally sign this check: ");
+            String paymentCheckSignature = keyboard.nextLine();
 
-    }
+            writeAction(formatDate1, formatTime2, paymentDescription, paymentVendor, paymentCheckAmount); /*Calls method to write to file*/
+            Thread.sleep(1500);
+            System.out.println("Your payment has been submitted. Have a great day!");
+            inmakePayment = false; /*Closes this whole thing*/
 
-    //    Deposit Screen (addDeposit Screen)------------------------------------------------------------------------
-    private static void addDeposit() {
-        boolean inaddDeposit = true;
-
-//      prompt user for the deposit information and save it to the csv file | For flavor, have them add acct number and signature
-        LocalDateTime today = LocalDateTime.now();
-
-//        Vendor format for CSV appendage - Establishing info/variables (date/time/description/vendor/amount) needed for appending later
-        String depositVendor = "Bank";
-//        Descriptor format for CSV appendage
-        String depositDescription = "Check Deposit";
-        //      Date format for CSV appendage
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        String formattedDate1 = today.format(formatter1);
-//      Time format for CSV appendage
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("hh:mm:ss");
-        String formattedDate2 = today.format(formatter2);
-
-        System.out.println("To deposit your check, please fill out the fields below.");
-
-//        Some flavor code was added. But the rest is required to add required info to CSV (Acct #/Rout #/Check #/Client Sig)
-        System.out.println("Check Account Number: ");
-        int checkAccountNumber = keyboard.nextInt();
-        System.out.println("Check Routing Number: ");
-        int checkRoutingNumber = keyboard.nextInt();
-        System.out.println("Check Number: ");
-        int checkNumber = keyboard.nextInt();
-        System.out.println("Check Amount: ");
-        double checkAmount = keyboard.nextInt();
-        System.out.println("And finally, type your name to digitally sign this check: ");
-        int checkSignature = keyboard.nextInt();
-//            switch (choice) {
+//    System.out.println("Check Account Number: ");/*Some flavor code was added. But the rest is required to add required info to CSV (Acct #/Rout #/Check #/Client Sig)*/
+//    int checkAccountNumber = keyboard.nextInt();/*May add as bonus later. Will keep separate to keep code simple*/
+//    System.out.println("Check Routing Number: ");
+//    int checkRoutingNumber = keyboard.nextInt();
+//    System.out.println("Check Number: ");
+//    int checkNumber = keyboard.nextInt();
+//    System.out.println("Check Amount: ");
+//    double checkAmount = keyboard.nextInt();
+//                switch (choice) {/*Below code is commented out at instructors guidance, may return and add as bonus*/
 //                case 'r' -> boolean inaddDeposit = false;
-//
 //            }
-
 //        switch (command){
 //            case ""
 //        }
@@ -166,12 +155,46 @@ public class App {
 //          if (trueFalseCheck == 0);
 //                run case 0;
 //            else
-//                append (formattedDate1 + formattedDate2 + depositDescription + depositVendor + checkAmount) to csv
+//                append (formattedDate1 + formattedDate2 + depositDescription + depositVendor + checkAmount) to csv | Save resources. Close your stuff out
+        }
+
+//         prompt user for the debit information and save it to the csv file
 
 
-//      Save resources. Close your stuff out
+    }
+
+    private static void writeAction(String formatDate1, String formatTime2, String paymentDescription, String paymentVendor, double paymentCheckAmount) {/*Established for above write action*/
+        LocalDateTime localDateTime = LocalDateTime.now();
+        try {
+            FileWriter writer = new FileWriter("TransactionExample.csv", true); /*This is the destination. Must also ensure that the append true is applied or will run into errors*/
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(formatDate1 + "|" + formatTime2 + "|" + paymentDescription + "|" + paymentVendor + "|" + paymentCheckAmount);/*Preps into format being used*/
+            bufferedWriter.newLine();
+            Transaction currentTransaction = new Transaction();
+            currentTransaction.setDate(localDateTime.toLocalDate());
+            currentTransaction.setTime(localDateTime.toLocalTime());
+            currentTransaction.setDescription(paymentDescription);
+            currentTransaction.setVendor(paymentVendor);
+            currentTransaction.setAmount(paymentCheckAmount);
+
+            transactions.add(currentTransaction);
+//            sortTransaction(); /*NEED TO MAKE METHOD*/
+            bufferedWriter.close();
+            writer.close();/*RECOMMENDED BUT IDK WHY*/
+        } catch (IOException e) {
+            System.out.println("Unexpected error");
+        }
+
+    }
+
+    private static void addDeposit() {/*Deposit Screen (addDeposit Screen)------------------------------------------------------------------------*/
+        boolean inaddDeposit = false;/*CHANGE TO TRUE WHEN READY TO WORK ON IT, for now it's false to prevent loop*/
+        while (inaddDeposit) {
+
 
 //        Method to combine all previous info and export to CSV goes here
+
+        }
 
 
     }
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,17 +1,52 @@
 package com.pluralsight;
 
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-public class Transaction {
-        public static void main(String[] args) {
+public class Transaction {/*Could move constructor up and remove overload*/
+        private LocalDate date;
+        private LocalTime time;
+        private String description;
+        private String vendor;
+        private double amount;
 
-
-
-
+        public LocalDate getDate() {
+                return date;
         }
-        private String date = "";
-        private String time = "";
-        private String description = "";
-        private String location = "";
-        private float amount = 0;
-    }
+
+        public void setDate(LocalDate date) {
+                this.date = date;
+        }
+
+        public LocalTime getTime() {
+                return time;
+        }
+
+        public void setTime(LocalTime time) {
+                this.time = time;
+        }
+
+        public String getDescription() {
+                return description;
+        }
+
+        public void setDescription(String description) {
+                this.description = description;
+        }
+
+        public String getVendor() {
+                return vendor;
+        }
+
+        public void setVendor(String vendor) {
+                this.vendor = vendor;
+        }
+
+        public double getAmount() {
+                return amount;
+        }
+
+        public void setAmount(double amount) {
+                this.amount = amount;
+        }
+}
